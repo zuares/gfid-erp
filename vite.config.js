59a565a9962330e33fa2778e56e875b0e51998ce
Arief Muhamad <arief.muhamadeu@icloud.com>
2026-07-18 05:02:18 +0700

    Revert chat to Vanilla JS (Remove React)

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/react/chat.jsx'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
-        react(),
     ],
 });
